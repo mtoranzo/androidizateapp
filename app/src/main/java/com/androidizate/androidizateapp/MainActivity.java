bf16223e035ca19f6f37e9701d685c1899db0286
mtoranzo <mtoranzo@gmail.com>
2017-07-24 20:51:13 -0300

    Primer commit

--- app/src/main/java/com/androidizate/androidizateapp/MainActivity.java
+++ app/src/main/java/com/androidizate/androidizateapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     Button btEMail;
     String mUserName;
     String mUserMail;
+
+    private static final int MAIL_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         tvMain.setText(String.format(getString(R.string.mensaje_bienvenida), mUserName, mUserMail));
 
-        btEMail = (Button)findViewById(R.id.bt_email);
+        btEMail = (Button) findViewById(R.id.bt_email);
         btEMail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void sendEmail(){
-        String[] mEmails = new String[] {mUserMail, "info@info.com"};
+    private void sendEmail() {
+        String[] mEmails = new String[]{mUserMail, "info@info.com"};
         String mSubject = "Saludo desde androidizate";
 
         Intent intent = new Intent(Intent.ACTION_SENDTO);
@@ -45,7 +48,20 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_EMAIL, mEmails);
         intent.putExtra(Intent.EXTRA_SUBJECT, mSubject);
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+//            startActivity(intent);
+            startActivityForResult(intent, MAIL_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MAIL_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "MENSAJE ENVIADO", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "ERROR DE ENVIO", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
