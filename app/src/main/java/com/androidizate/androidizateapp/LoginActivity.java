bddf7a3c3c27dcb3a96e4586377834b47dbe6a16
mtoranzo <mtoranzo@gmail.com>
2017-07-24 19:29:44 -0300

    Primer commit

---
+++ app/src/main/java/com/androidizate/androidizateapp/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.androidizate.androidizateapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Alumno on 24/07/2017.
+ */
+
+public class LoginActivity extends Activity {
+
+    EditText etUserName;
+    EditText etUserEmail;
+    EditText etUserPassword;
+    Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUserName = (EditText) findViewById(R.id.et_user_name);
+        etUserEmail = (EditText) findViewById(R.id.et_user_mail);
+        etUserPassword = (EditText) findViewById(R.id.et_user_password);
+        btLogin = (Button) findViewById(R.id.bt_login);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validLogin())
+                    doLogin();
+            }
+        });
+    }
+
+    private boolean validLogin() {
+        if (etUserName.getText().toString().trim().isEmpty()) {
+            etUserName.setError("USUARIO VACIO");
+            return false;
+        }
+        if (etUserEmail.getText().toString().trim().isEmpty()) {
+            etUserEmail.setError("EMAIL VACIO");
+            return false;
+        }
+        if (etUserPassword.getText().toString().trim().isEmpty()) {
+            etUserPassword.setError("PASSWORD VACIO");
+            return false;
+        }
+        if (etUserPassword.getText().toString().trim() != "1234") {
+            etUserPassword.setError("PASSWORD INVALIDO");
+            return false;
+        }
+        return true;
+    }
+
+    private void doLogin() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra("user_name", etUserName.getText());
+        intent.putExtra("user_mail", etUserEmail.getText());
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/androidizate/androidizateapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.androidizate.androidizateapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMain = (TextView) findViewById(R.id.tv_main);
+
+        String mUserName = getIntent().getStringExtra("user_name");
+        String mUserMail = getIntent().getStringExtra("user_mail");
+
+        tvMain.setText("Bienvenido " + mUserName + "!!! Pordés empezar a enviar correos con tu mail: " + mUserMail);
+    }
+}
